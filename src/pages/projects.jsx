e3f1069a9c584879040446984e872574757f43eb
Jurvis Tan <jurvis.tan@me.com>
2018-04-07 18:01:19 +0800

    use ProjectDetail component in projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import styled from 'tachyons-components';
+import ProjectDetail from '../components/ProjectDetail'
 
 const ContentContainer = styled('div')`
-  flex flex-column w-80
 `;
 
 class ProjectsPage extends React.Component {
   render() {
+    const projects = this.props.data.allProjectsJson.edges;
+
     return (
       <ContentContainer>
-        This is the projects page
+        {projects.map( projectNode => {
+          const project = projectNode.node;
+          return <ProjectDetail key={project.id} project={project}/>
+        })}
       </ContentContainer>
     )
   }
 }
 
 export default ProjectsPage
+
+export const query = graphql`
+  query ProjectsQuery {
+    allProjectsJson {
+      edges {
+        node {
+          id
+          name
+          skills
+          image_dir
+        }
+      }
+    }
+  }
+`;
+
