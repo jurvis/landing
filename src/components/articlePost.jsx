f8a0df37a6cd8155d554e37918f1dd24f6b266aa
Jurvis Tan <jurvis.tan@me.com>
2018-11-18 19:11:44 +0800

    move article styled component to it’s own file

---
+++ src/components/articlePost.jsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+const Article = styled.article`
+  display: flex;
+  flex-direction: column;
+  width: 92%;
+  padding: 0 15px;
+  margin: 0 auto;
+
+  h1, h2, h3, h4, h5, h6 {
+    color: #161718;
+    line-height: 1.1;
+    margin: 0 0 1rem;
+
+  }
+
+  h1 {
+    font-size: 2em;
+
+    @media only screen and (min-width: 700px) {
+      font-size: 3em;
+    }
+  }
+
+  h2 {
+    font-size: 1.3rem;
+
+    @media only screen and (min-width: 700px) {
+      font-size: 2rem;
+    }
+  }
+
+  h3 {
+    font-size: 1em;
+
+    @media only screen and (min-width: 700px) {
+      font-size: 1.5em;
+    }
+  }
+
+  h4 {
+    font-size: 1em;
+
+    @media only screen and (min-width: 700px) {
+      font-size: 1.4em;
+    }
+  }
+
+  p {
+    font-size: 18px;
+    line-height: 1.75;
+    margin: 0 0 2rem;
+  }
+  
+  @media only screen and (min-width: 700px) {
+    width: 40em;
+  }
+`
+
+export default Article
+
--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -1,9 +1,10 @@
 import { Component } from 'react';
 import React from 'react';
-import Helmet from 'react-helmet'
-import styled from 'styled-components'
+import Helmet from 'react-helmet';
+import styled from 'styled-components';
 
-import Header from '../components/header.js'
+import Header from '../components/header.js';
+import Article from '../components/articlePost.jsx';
 
 const Container = styled.div`
 `
@@ -36,63 +37,6 @@ const ArticleHeader = styled.header`
     }
   }
 
-  @media only screen and (min-width: 700px) {
-    width: 40em;
-  }
-`
-
-const Article = styled.article`
-  display: flex;
-  flex-direction: column;
-  width: 92%;
-  padding: 0 15px;
-  margin: 0 auto;
-
-  h1, h2, h3, h4, h5, h6 {
-    color: #161718;
-    line-height: 1.1;
-    margin: 0 0 1rem;
-
-  }
-
-  h1 {
-    font-size: 2em;
-
-    @media only screen and (min-width: 700px) {
-      font-size: 3em;
-    }
-  }
-
-  h2 {
-    font-size: 1.3rem;
-
-    @media only screen and (min-width: 700px) {
-      font-size: 2rem;
-    }
-  }
-
-  h3 {
-    font-size: 1em;
-
-    @media only screen and (min-width: 700px) {
-      font-size: 1.5em;
-    }
-  }
-
-  h4 {
-    font-size: 1em;
-
-    @media only screen and (min-width: 700px) {
-      font-size: 1.4em;
-    }
-  }
-
-  p {
-    font-size: 18px;
-    line-height: 1.75;
-    margin: 0 0 2rem;
-  }
-  
   @media only screen and (min-width: 700px) {
     width: 40em;
   }
