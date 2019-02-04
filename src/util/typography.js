6c975cd341246c4da878ccfc397906b97f864869
Jurvis Tan <jurvis.tan@me.com>
2019-02-04 16:22:46 +0800

    fix typography.js overrides

--- src/util/typography.js
+++ src/util/typography.js
@@ -4,7 +4,14 @@ const typography = new Typography({
   headerFontFamily: ["-apple-system", "BlinkMacSystemFont","Roboto","Helvetica Neue","Helvetica","Arial","sans-serif"], 
   bodyFontFamily: ["-apple-system", "BlinkMacSystemFont","Roboto","Helvetica Neue","Helvetica","Arial","sans-serif"],
   baseFontSize: '16px',
-  baseLineHeight: 1.75,
+  baseLineHeight: 1.7,
+  overrideStyles: () => ({
+    blockquote: {
+      borderLeft: `.25rem solid #408bc9`,
+      margin: `1rem 0 2rem 1rem`,
+      paddingLeft: `1rem`,
+    }
+  })
 });
 
 export default typography
