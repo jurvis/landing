631a83703450e4b9234d8907da0f6bc640c0dfa2
Jurvis Tan <jurvis.tan@me.com>
2019-09-08 22:45:49 +0800

    set rss feed title

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
               }
             `,
             output: "/rss.xml",
-            title: "Your Site's RSS Feed",
+            title: "jurv.is",
           },
         ],
       }
